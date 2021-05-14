7b435a8bfe8ebbe90c05f50d1c428c71cfc9dae1
rudy3091 <grayblack313@gmail.com>
2021-05-15 01:56:36 +0900

    Add AddTodo component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="container">
+		<svg width="30" height="30" viewBox="0 0 100 100" @click="handleClick">
+			<g>
+				<line x1="50" y1="0" x2="50" y2="100"></line>
+				<line x1="0" y1="50" x2="100" y2="50"></line>
+			</g>
+		</svg>
+
+		<div class="input-wrapper" style="display: {{ isVisible }}">
+			<input type="text" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "AddTodo",
+	data() {
+		return {
+			hidden: true,
+		};
+	},
+	methods: {
+		handleClick() {
+			console.log(this.hidden);
+			this.hidden = !this.hidden;
+		},
+	},
+	computed: {
+		isVisible() {
+			return this.hidden ? "none" : "block";
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/mixins.scss";
+@import "../styles/colors.scss";
+
+.container {
+	@include flex-center;
+	flex-direction: column;
+	width: 100%;
+
+	svg {
+		margin: 25px;
+		& > g {
+			fill: none;
+			stroke-width: 15px;
+			stroke: $font-color;
+		}
+	}
+
+	.input-wrapper {
+		width: 100%;
+		height: 30px;
+
+		input {
+			width: 100%;
+			height: 100%;
+			border: none;
+			background-color: #f2f2f2;
+			border-radius: 4px;
+		}
+	}
+}
+</style>
--- src/components/TodoContainer.vue
+++ src/components/TodoContainer.vue
@@ -2,15 +2,20 @@
 	<section class="todo-container">
 		<TodoItem name="go to sleep" due="210514" />
 	</section>
+	<section class="todo-create">
+		<AddTodo />
+	</section>
 </template>
 
 <script>
 import TodoItem from "./TodoItem.vue";
+import AddTodo from "./AddTodo.vue";
 
 export default {
 	name: "TodoContainer",
 	components: {
 		TodoItem,
+		AddTodo,
 	},
 };
 </script>
@@ -22,5 +27,8 @@ export default {
 .todo-container {
 	padding: 20px;
 	border: 1px solid $font-color;
+}
+
+.todo-create {
 }
 </style>
