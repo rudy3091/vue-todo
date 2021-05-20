89ae8ec3f3af834dcd7ed56bfe8e984c272a614f
rudy3091 <grayblack313@gmail.com>
2021-05-20 17:25:05 +0900

    Add Tooltip component

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="tooltip-container" :class="{ show }">{{ content }}</div>
+</template>
+
+<script>
+export default {
+	name: "Tooltip",
+	props: {
+		content: String,
+		show: Boolean,
+	},
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/mixins.scss";
+@import "../styles/colors.scss";
+
+.tooltip-container {
+	font-size: 0.75rem;
+	font-weight: 600;
+	padding: 8px 12px;
+	color: $bg-color-main;
+	border-radius: 4px;
+	position: absolute;
+	top: -40px;
+	background-color: $font-color;
+	opacity: 0;
+	transition: opacity 0.2s ease;
+
+	&.show {
+		opacity: 1;
+	}
+}
+</style>
